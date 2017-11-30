c1bb0bc4bb857e3cb80c1ffd4ca294a6c19c5d41
pjastr <24464854+pjastr@users.noreply.github.com>
2017-11-30 18:40:42 +0100

    Add files via upload

---
+++ ConsoleApplication31/ConsoleApplication31/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface ISport
+    {
+        string Graj(int min);
+    }
+
+    class Pilkarz:ISport
+    {
+
+        public string Graj(int min)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApplication32/ConsoleApplication32/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new NaszWyjatekException("ddddd");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Więcej pomocy na {0}", e.HelpLink);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class NaszWyjatekException:Exception
+    {
+        public NaszWyjatekException()
+        {
+
+        }
+
+        public NaszWyjatekException(string message)
+            : base(message) 
+        {
+            HelpLink = "http://www.google.pl/";
+        }
+
+        public NaszWyjatekException(String message, Exception innerException)
+            : base(message, innerException) { }
+
+    }
+}
